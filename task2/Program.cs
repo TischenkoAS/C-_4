541577c9713afc354f54390122aec0c8d788a75e
«anastasiatischenko» <1510anastasia@mail.ru>
2023-02-09 16:51:36 +0300

    Добавили task1 и task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!)!;
+
+Console.WriteLine($"Произведение чисел равно {GetMulty(num)}.");
+
+int GetMulty(int number)
+{
+    int temp = 1;
+    for (int count = 1; count <= number; count++)
+    {
+        temp *= count;
+    }
+    return temp;
+}
