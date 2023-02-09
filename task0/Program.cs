c92c6ea5247bde0e0a620ff718ce80c88c33035b
«anastasiatischenko» <1510anastasia@mail.ru>
2023-02-09 15:35:36 +0300

    Добавили task0

---
+++ task0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!)!;
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number;
+        number--;
+    }
+    return sum;
+}
